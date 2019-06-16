38035d00af2491c8ee8014ebdee98445822ce9f0
Thiago Henrique <thiagonascimento95@hotmail.com>
2019-06-16 15:09:41 -0300

    Adding express

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,22 @@
-console.log('Hello World');
+import * as express from 'express';
 
-console.log('nodemon');
+class App {
+
+    public express: express.Application;
+
+    constructor() {
+        this.express = express();
+        this.middleware();
+    }
+
+    private middleware(): void {
+        this.express.use('/hello', (req, res, next) => {
+            res.send({
+                hello: 'Hello world!'
+            });
+        });
+    }
+
+}
+
+export default new App().express;
